ec8280f770e8fa18db8518b84da203b6610fa295
TulparN <cetin.ab@outlook.com>
2021-06-06 00:56:21 +0300

    5 haziran React Kısmı

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./layouts/Dashboard";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+import Navigation from "./layouts/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navigation/>
+      <Container className="main">
+        <Dashboard />
+      </Container>
     </div>
   );
 }
